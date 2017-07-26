3356af36f9df5eb2c27a1841d298b48395467cc1
mechamanny <ekaunitz@gmail.com>
2017-07-26 18:10:08 -0400

    MVP for change maker UI

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def make_change
+    @change = helpers.calculate_change(params[:denom].to_sym, params[:money_value].to_i)
+    respond_to do |format|
+      format.js
+    end
+  end
 end
